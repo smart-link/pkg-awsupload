bd90062c568615597386193d0203e1792b9260dc
David Kim <david.kim@smartlink.io>
2017-02-11 00:28:58 +0900

    v0.1.0: git URL added

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Modify slingshot in order to make it easy to upload image files to AWS.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/smart-link/pkg-awsupload',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
